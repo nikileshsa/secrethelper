eba7ecf7743b4c5110b2e704beb16bd78cacc0d9
paujim <paujim@hotmail.com>
2020-02-24 05:26:49 +1100

    Removed testing code

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/aws/aws-lambda-go/cfn"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -30,27 +29,5 @@ func handler(ctx context.Context, event cfn.Event) (physicalResourceID string, d
 }
 
 func main() {
-	_, resp, err := handler(nil, cfn.Event{
-		ResourceType:       "Custom::RSAKey",
-		ResourceProperties: map[string]interface{}{"Name": "/KEY_RSA"}})
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	data, ok := resp["Response"].(responseSecret)
-	if ok {
-		publicKey := *data.PublicKey
-		_, resp, err = handler(nil, cfn.Event{
-			ResourceType: "Custom::KeyPair",
-			ResourceProperties: map[string]interface{}{
-				"Name":      "KEYSSM",
-				"PublicKey": publicKey,
-			}})
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Print(resp["Response"])
-	}
 	lambda.Start(cfn.LambdaWrap(handler))
 }
